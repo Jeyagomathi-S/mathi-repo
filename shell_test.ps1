2433565cef34bc20564509b4c59a0bc957b55cf7
Jeyagomathi-S <58843498+Jeyagomathi-S@users.noreply.github.com>
2019-12-19 12:36:35 +0530

    Update shell_test.ps1

--- shell_test.ps1
+++ shell_test.ps1
@@ -1,5 +1,5 @@
 ﻿$host_name=hostname
-$test= $($env:USERNAME)
+$test= "Welcome from GIT HUB"
 new-item -itemtype "directory" c:\jen -Force
 $host_name | out-file "c:\jen\test.txt" -Force
 $test | Out-File "c:\jen\test.txt" -Append
